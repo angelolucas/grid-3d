6a43c320f62b83b50b5f955cc38a08cc371f2222
Ângelo Lucas <angelo.lrc@gmail.com>
2017-04-29 16:09:57 -0300

    rotateY and Z

--- javascript/tools.js
+++ javascript/tools.js
@@ -1,12 +1,41 @@
+// Default Values
+var transform = {
+  translateX: 100, // %
+  rotateX: 0, // deg
+  rotateY: 0, // deg
+  rotateZ: 0, // deg
+}
 
-var translateX = 'translateX(100%)';
+// Define Default Values;
+$('.tools__input-rotateX').val(transform.rotateX);
+$('.tools__input-rotateY').val(transform.rotateX);
+$('.tools__input-rotateZ').val(transform.rotateX);
 
-$('input').on('input', function(e){
-
-  $('input').val($(this).val());
-
-  var rotateX = "rotateX(" + $(this).val() + "deg";
-
-  $('.grid-3d div').css( "transform", translateX + rotateX );
-
+$('.tools__input-rotateX').on('input', function(){
+  $('.tools__input-rotateX').val($(this).val());
+  transform.rotateX = $(this).val();
+  changeGrid();
 })
+
+$('.tools__input-rotateY').on('input', function(){
+  $('.tools__input-rotateY').val($(this).val());
+  transform.rotateY = $(this).val();
+  changeGrid();
+})
+
+$('.tools__input-rotateZ').on('input', function(){
+  $('.tools__input-rotateZ').val($(this).val());
+  transform.rotateZ = $(this).val();
+  changeGrid();
+})
+
+changeGrid();
+
+function changeGrid() {
+  $('.grid-3d div').css( 'transform',
+    'translateX(' + transform.translateX + '%)' +
+    'rotateX(' + transform.rotateX + 'deg)' +
+    'rotateY(' + transform.rotateY + 'deg)' +
+    'rotateZ(' + transform.rotateZ + 'deg)'
+  );
+}
