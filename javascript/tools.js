b29272de0cd9adeb3341d290782b1b4ad6b3e2cc
Ângelo Lucas <angelo.lrc@gmail.com>
2017-05-01 16:36:05 -0300

    add block function

--- javascript/tools.js
+++ javascript/tools.js
@@ -14,12 +14,25 @@ var block = {
 var create = function() {
   var blocks = '<div>';
 
-  for(var i = 0; i < block.length; i++) {
+  for(var i = 1; i < block.length -1; i++) {
     blocks += '<div>';
   }
+  blocks += '<div class="last">';
 
   $('.grid-3d').append(blocks);
 }
+
+// Add
+$('.tool__add').on('click', function() {
+  var $last = $('.grid-3d .last');
+
+  $last.append('<div>');
+  $last.removeClass('last');
+  $last.find('div').addClass('last');
+
+  transformGrid();
+
+});
 
 create();
 
@@ -31,25 +44,24 @@ $('.tools__input-rotateZ').val(transform.rotateX);
 $('.tools__input-rotateX').on('input', function(){
   $('.tools__input-rotateX').val($(this).val());
   transform.rotateX = $(this).val();
-  changeGrid();
+  transformGrid();
 })
 
 $('.tools__input-rotateY').on('input', function(){
   $('.tools__input-rotateY').val($(this).val());
   transform.rotateY = $(this).val();
-  changeGrid();
+  transformGrid();
 })
 
 $('.tools__input-rotateZ').on('input', function(){
   $('.tools__input-rotateZ').val($(this).val());
   transform.rotateZ = $(this).val();
-  changeGrid();
+  transformGrid();
 })
 
+transformGrid();
 
-changeGrid();
-
-function changeGrid() {
+function transformGrid() {
   $('.grid-3d div').css( 'transform',
     'translateX(' + transform.translateX + '%)' +
     'rotateX(' + transform.rotateX + 'deg)' +
