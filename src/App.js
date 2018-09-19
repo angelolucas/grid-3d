f594e586692d6f60c8ad490047b34524ec13ebf9
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-19 17:16:12 -0300

    Rotate Component, step2

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import React, { Component } from 'react';
-import Axis from './components/Axis';
-import './App.css';
+import React, { Component } from "react";
+import Axis from "./components/Axis";
+import Rotate from "./components/Rotate";
+import "./App.css";
 
 class App extends Component {
-  state = {
-    startPoint: false, // horizontal, vertical
-    rotate: [0, 0]
-  };
-
-  startRotate = e => {
-    this.setState({ startPoint: [e.pageX, e.pageY] });
-  };
-
-  handleRotate = e => {
-    const { startPoint } = this.state;
-
-    if (startPoint) {
-      const measureFromStartPoint = [
-        e.pageX - startPoint[0],
-        e.pageY - startPoint[1]
-      ];
-
-      const rotate = [
-        this.state.startPoint[0] + measureFromStartPoint[0],
-        this.state.startPoint[1] + measureFromStartPoint[1]
-      ];
-
-      this.setState({ rotate });
-    }
-  };
-
-  stopRotate = () => {
-    this.setState({ startPoint: false });
-  };
-
   render() {
     return (
-      <div
-        className="App"
-        onMouseDown={e => this.startRotate(e)}
-        onMouseMove={e => this.handleRotate(e)}
-        onMouseUp={() => this.stopRotate()}
-      >
-        <div
-          ref={object => (this.object = object)}
-          className="object-container"
-          style={{
-            transform: `rotateX(${this.state.rotate[1]}deg) rotateZ(${
-              this.state.rotate[0]
-            }deg) `
-          }}
-        >
+      <div className="App">
+        <Rotate className="object-container">
+          <div style={{ width: 100, height: 100, backgroundColor: "black" }} />
           <Axis />
-        </div>
+        </Rotate>
       </div>
     );
   }
---
+++ src/components/Rotate.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Rotate extends Component {
+  state = {
+    x: 0,
+    z: 0
+  };
+  componentDidMount() {
+    this.handleRotate(this.object);
+  }
+
+  handleRotate = object => {
+    object.onmousedown = e => {
+      let dragging = true;
+      let prevX = e.pageX;
+      let prevY = e.pageY;
+
+      object.onmousemove = e => {
+        if (dragging) {
+          const measureFromStartX = e.pageX - prevX;
+          const measureFromStartY = e.pageY - prevY;
+
+          prevX = e.pageX;
+          prevY = e.pageY;
+
+          const x = this.state.x + (measureFromStartY * 100) / 360;
+          const z = this.state.z + (measureFromStartX * 100) / 360;
+
+          this.setState({ x, z });
+        }
+      };
+
+      object.onmouseup = () => {
+        dragging = false;
+      };
+    };
+  };
+
+  render() {
+    const { children, ...rest } = this.props;
+    const { x, z } = this.state;
+    const transform = {
+      transform: `rotateX(${x}deg) rotateZ(${z}deg)`
+    };
+
+    return (
+      <div {...rest} ref={object => (this.object = object)}>
+        <div style={transform}>{children}</div>
+      </div>
+    );
+  }
+}
+
+export default Rotate;
