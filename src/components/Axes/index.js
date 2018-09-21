c2082cc83217f859f16fe310c15cf736850bd528
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-20 21:41:07 -0300

    Axis with parent measures

--- src/components/Axes/index.js
+++ src/components/Axes/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Axis from './Axis'
+import { StyleSheet, css } from 'aphrodite/no-important'
 
 class Axes extends Component {
   render() {
     return (
-      <div>
+      <div className={css(styles.root)}>
         <Axis name="x" hexColor="#ff0000" />
         <Axis name="y" hexColor="#00ff00" />
         <Axis name="z" hexColor="#0000ff" />
@@ -12,5 +13,13 @@ class Axes extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+  },
+})
 
 export default Axes
